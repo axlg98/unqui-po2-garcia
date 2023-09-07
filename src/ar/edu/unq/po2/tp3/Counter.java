354952f30cfe80a579eca6716433966d7ea21894
axlg98 <axlgarcia080498@gmail.com>
2023-09-07 12:32:46 -0300

    avanzando tp3

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,41 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+
+public class Counter {
+private ArrayList<Integer> listaDeNumeros = new ArrayList<Integer>();
+	
+	public void addNumber(Integer x) {
+		listaDeNumeros.add(x);
+	}
+	
+	public int contarPares() {
+		int cantidad = 0;
+		for(Integer numero:listaDeNumeros) {
+			if(numero % 2 == 0) {
+				cantidad += 1;
+			}
+		}
+		return cantidad;
+	}
+	
+	public int contarImpares() {
+		int cantidadImp = 0;
+		for(Integer numero:listaDeNumeros) {
+			if(numero % 2 > 0) {
+				cantidadImp += 1;
+			}
+		}
+		return cantidadImp;
+	}
+	
+	public int contarMultiplos(int x) {
+		int cantiMult = 0;
+		for(Integer numero:listaDeNumeros) {
+			if(numero % x == 0) {
+				cantiMult +=1;
+			}
+		}
+		return cantiMult;
+	}
+}
