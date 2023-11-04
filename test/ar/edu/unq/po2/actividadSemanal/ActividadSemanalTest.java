232ee5f3d38f8411cfd544386226e775c5ef4442
axlg98 <axlgarcia080498@gmail.com>
2023-11-04 02:29:47 -0300

    .

---
+++ test/ar/edu/unq/po2/actividadSemanal/ActividadSemanalTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.actividadSemanal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ActividadSemanalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/ar/edu/unq/po2/bancoYPrestamo/BancoYPrestamoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.bancoYPrestamo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BancoYPrestamoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
